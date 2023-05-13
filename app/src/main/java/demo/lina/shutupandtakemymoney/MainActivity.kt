44d4479a12f538b72319f1464427fc75a1cbf365
dalaoqi <mystes3016@gmail.com>
2023-05-13 16:29:58 +0800

    WIP

--- app/src/main/java/demo/lina/shutupandtakemymoney/MainActivity.kt
+++ app/src/main/java/demo/lina/shutupandtakemymoney/MainActivity.kt
@@ -6,12 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,11 +62,73 @@ fun MainPage(modifier: Modifier = Modifier) {
             Record("排球場地費", 150, "運動"),
             Record("溜冰", 350, "娛樂"),
         )
+        Column() {
+            addItem(recordList)
+            ShowRecordList(recordList)
+        }
 
-        ShowRecordList(recordList)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun addItem(recordList: MutableList<Record>){
+    Row(
+        Modifier
+            .padding(16.dp, 0.dp)
+            .height(84.dp), verticalAlignment = Alignment.CenterVertically) {
+        var itemName by remember { mutableStateOf("") }
+        var itemPrice by remember { mutableStateOf("") }
+        TextField(
+            value = itemName,
+            onValueChange = { newText ->
+                itemName = newText
+            },
+            Modifier
+                .padding(5.dp)
+                .size(40.dp)
+                .weight(1f)
+        )
+        TextField(
+            value = itemPrice,
+            onValueChange = { newText ->
+                itemPrice = newText
+            },
+            Modifier
+                .padding(5.dp)
+                .size(40.dp)
+                .weight(1f)
+        )
+        Button(shape= RoundedCornerShape(10.dp),onClick = {
+            if (itemName!="" && itemPrice != "") {
+                recordList.add(Record(itemName, itemPrice.toInt(), "日用品"))
+
+            }
+        }, modifier = Modifier
+            .padding(15.dp, 0.dp, 0.dp,0.dp)
+            .size(50.dp)
+            .weight(1f)
+        ) {
+            Text("Add")
+        }
+    }
+}
+
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun RowScope.TextFieldItem(hint: String) {
+//    var searchText by remember { mutableStateOf("") }
+//    TextField(
+//        value = searchText,
+//        onValueChange = { newText ->
+//            searchText = newText
+//        },
+//        Modifier
+//            .padding(5.dp)
+//            .size(40.dp)
+//            .weight(1f)
+//    )
+//}
 @Composable
 private fun ShowRecordList(recordList: MutableList<Record>,
                            modifier: Modifier = Modifier) {
@@ -68,7 +143,9 @@ private fun ShowRecordList(recordList: MutableList<Record>,
 private fun ShowRecord(record: Record, modifier: Modifier = Modifier) {
 
     Row(
-        modifier = modifier.fillMaxWidth().padding(10.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(10.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
@@ -87,7 +164,7 @@ private fun ShowRecord(record: Record, modifier: Modifier = Modifier) {
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun DefaultPreview() {
     ShutUpAndTakeMyMoneyTheme {
