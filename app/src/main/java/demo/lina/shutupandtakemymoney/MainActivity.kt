84c6fa1f9eadb13431afff25a482c93a67346c0f
dalaoqi <mystes3016@gmail.com>
2023-05-17 00:11:40 +0800

    user can add something by main page

--- app/src/main/java/demo/lina/shutupandtakemymoney/MainActivity.kt
+++ app/src/main/java/demo/lina/shutupandtakemymoney/MainActivity.kt
@@ -1,6 +1,10 @@
 package demo.lina.shutupandtakemymoney
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -9,22 +13,34 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,17 +67,7 @@ fun MainPage(modifier: Modifier = Modifier) {
     ) {
         // rememberSaveable: can save the state while activity restart
         // var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-        val recordList = mutableListOf<Record>(
-            Record("早餐", 50, "食物"),
-            Record("便當", 100, "食物"),
-            Record("晚餐", 65, "食物"),
-            Record("餅乾", 135, "食物"),
-            Record("手搖", 55, "食物"),
-            Record("洗衣網", 198, "日用品"),
-            Record("棉條", 505, "日用品"),
-            Record("排球場地費", 150, "運動"),
-            Record("溜冰", 350, "娛樂"),
-        )
+        val recordList = remember { mutableStateListOf<Record>()}
         Column() {
             addItem(recordList)
             ShowRecordList(recordList)
@@ -70,7 +76,7 @@ fun MainPage(modifier: Modifier = Modifier) {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun addItem(recordList: MutableList<Record>){
     Row(
@@ -79,35 +85,49 @@ fun addItem(recordList: MutableList<Record>){
             .height(84.dp), verticalAlignment = Alignment.CenterVertically) {
         var itemName by remember { mutableStateOf("") }
         var itemPrice by remember { mutableStateOf("") }
-        TextField(
+        val keyboardController = LocalSoftwareKeyboardController.current
+        val focusManager = LocalFocusManager.current
+        OutlinedTextField(
             value = itemName,
-            onValueChange = { newText ->
-                itemName = newText
+            onValueChange = { newValue ->
+                itemName = newValue
             },
-            Modifier
+            modifier = Modifier
                 .padding(5.dp)
-                .size(40.dp)
-                .weight(1f)
+                .weight(1f),
+            label = { Text("項目名稱") },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
         )
-        TextField(
-            value = itemPrice,
-            onValueChange = { newText ->
-                itemPrice = newText
-            },
-            Modifier
-                .padding(5.dp)
-                .size(40.dp)
-                .weight(1f)
-        )
-        Button(shape= RoundedCornerShape(10.dp),onClick = {
-            if (itemName!="" && itemPrice != "") {
-                recordList.add(Record(itemName, itemPrice.toInt(), "日用品"))
 
+        OutlinedTextField(
+            value = itemPrice,
+            onValueChange = { newValue ->
+                itemPrice = newValue
+            },
+            modifier = Modifier
+                .padding(5.dp)
+                .weight(1f),
+            label = { Text("價格") },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+        )
+
+        Button(shape= RoundedCornerShape(10.dp),onClick = {
+                if (itemName.isNotEmpty() && itemPrice.isNotEmpty()) {
+                    recordList.add(Record(itemName, itemPrice.toInt(), "日用品"))
+                    itemName = "" // 清空輸入的項目名稱
+                    itemPrice = "" // 清空輸入的價格
+                    recordList.forEach { record ->
+                        Log.d("RecordList", record.toString())
+                }
             }
+
         }, modifier = Modifier
             .padding(15.dp, 0.dp, 0.dp,0.dp)
             .size(50.dp)
             .weight(1f)
+
         ) {
             Text("Add")
         }
@@ -164,21 +184,21 @@ private fun ShowRecord(record: Record, modifier: Modifier = Modifier) {
 }
 
 
-@Preview(showBackground = false)
-@Composable
-fun DefaultPreview() {
-    ShutUpAndTakeMyMoneyTheme {
-        val recordList = mutableListOf(
-            Record("早餐", 50, "食物"),
-            Record("便當", 100, "食物"),
-            Record("晚餐", 65, "食物"),
-            Record("餅乾", 135, "食物"),
-            Record("手搖", 55, "食物"),
-            Record("洗衣網", 198, "日用品"),
-            Record("棉條", 505, "日用品"),
-            Record("排球場地費", 150, "運動"),
-            Record("溜冰", 350, "娛樂"),
-        )
-        ShowRecordList(recordList)
-    }
-}
+//@Preview(showBackground = false)
+//@Composable
+//fun DefaultPreview() {
+//    ShutUpAndTakeMyMoneyTheme {
+//        val recordList = mutableListOf(
+//            Record("早餐", 50, "食物"),
+//            Record("便當", 100, "食物"),
+//            Record("晚餐", 65, "食物"),
+//            Record("餅乾", 135, "食物"),
+//            Record("手搖", 55, "食物"),
+//            Record("洗衣網", 198, "日用品"),
+//            Record("棉條", 505, "日用品"),
+//            Record("排球場地費", 150, "運動"),
+//            Record("溜冰", 350, "娛樂"),
+//        )
+//        ShowRecordList(recordList)
+//    }
+//}
